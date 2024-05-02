de0f5da6b9e441a76ab28557ee91864b1ba617d7
Thahmidur-R <109155385+Thahmidur-R@users.noreply.github.com>
2024-05-02 21:14:21 +0100

    Update Sidebar.jsx

--- frontend/src/scenes/global/Sidebar.jsx
+++ frontend/src/scenes/global/Sidebar.jsx
@@ -2,17 +2,14 @@ import { useState } from "react";
 import {ProSidebar, Menu, MenuItem} from "react-pro-sidebar";
 import "react-pro-sidebar/dist/css/styles.css";
 import {Box, IconButton, Typography, useTheme} from '@mui/material';
-import {Link} from "react-router-dom"; //allows us to link when we click on navigation item so it takes us to relevant page
+import {Link} from "react-router-dom"; 
 import {tokens} from "../../theme";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import ClassOutlinedIcon from '@mui/icons-material/ClassOutlined';
 import ChecklistIcon from '@mui/icons-material/Checklist';
-import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
 import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
-import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
-import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 
 
